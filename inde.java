f8a432c8426c27c202edad9d3b2a971cfd8d06f1
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-09-02 11:22:55 +0530

    Create inde.java

---
+++ inde.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class index
+{
+public static void main(String h[])
+{
+		int a[]=new int[]{-1,1,2,-2,3,5},i;
+Arrays.sort(a);
+for(i=0;i<a.length;i++)
+ if(i==a[i])
+  System.out.println(a[i]);
+}
+}
