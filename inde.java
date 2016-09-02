90633f330bbec604dfdced0439c6d4dcdd658f31
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-09-02 11:23:53 +0530

    Update inde.java

--- inde.java
+++ inde.java
@@ -3,7 +3,7 @@ class index
 {
 public static void main(String h[])
 {
-		int a[]=new int[]{-1,1,2,-2,3,5},i;
+int a[]=new int[]{-1,1,2,-2,3,5},i;
 Arrays.sort(a);
 for(i=0;i<a.length;i++)
  if(i==a[i])
